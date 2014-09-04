f2919a692d819aad9d27ad45507744f3bc582666
Blieque Mariguan <himself@blieque.co.uk>
2014-09-05 00:44:44 +0100

    work on contact page form

--- jquery.fs.js
+++ jquery.fs.js
@@ -1,49 +1,22 @@
-/* FUNCTIONAL STONEWARE
- * JAVASCRIPT (w/JQUERY) */
+/* functional stoneware
+ * javascript (w/jQuery)
+ * written by blieque mariguan
+ * licensed under GPL v2 */
 
 // variables
-var menuOpen = 0;
+var _placehold;
 
 // init
 function init() {
 }
-function menuCtl(onlyIf) {
-	if (onlyIf == menuOpen || onlyIf == undefined) {
-		if (menuOpen) {
-			// $("#wrap>*").animate({opacity:1},250);
-			$("#side>a").animate({left:"0em"},10000,function(){
-				// $("#side,#content").animate({left:0},250);
-				// menuOpen = 0;
-			});
-		} else {
-			// $("#wrap>*").animate({opacity:.75},250);
-			$("#side").animate({left:"16em"},250);
-			$("#content").animate({left:"3em"},250,function(){
-				$("#side>a").animate({left:"-4em"},250);
-				menuOpen = 1;
-			});
-		}
-	}
+function formTick(d,s) {						// data, status
+	console.log(s);
+	// svg animation yo
 }
 
-// menu at left
-
-// jquery
+// jQ call
 $(function(){
-	init();
-
-	// click events
-	$("#side a").click(function(){
-		menuCtl();
+	$("[type='submit']").click(function(){
+		$.post("contact","a=s&" + $("form").serialize(),formTick);
 	});
-	$("#content").click(function(){
-		menuCtl(1);
-	});
-
-	// keyboard events
-	$("body").keydown(function(e){
-		if (e.which == 77) {
-			menuCtl();
-		}
-	})
 });
--- pages/contact.php
+++ pages/contact.php
@@ -1,3 +1,10 @@
-<?php include"../_cgi-bin/builder.php";fs_open("Contact")?>
-<p>Formz up in here</p>
-<?php fs_close()?>
+<?php if ($_POST['a'] == "s") {
+	echo "gettin' hit up";
+	echo $_POST["name"] . $_POST["mail"] . $_POST["text"];
+} else { include"../_cgi-bin/builder.php";fs_open("Contact")?>
+<form method="post"><input name="name" placeholder="Name" type="text"><input name="mail" placeholder="Email Address" type="text"><textarea name="text" placeholder="Message" rows="12"></textarea><div type="submit">SUBMIT
+<svg width="30" height="24">
+	<path id="e" d="M8 2L28 22M28 2L8 22"/>
+	<path id="s" d="M2 16L8 22L28 2"/>
+</svg>
+</div></form><?php fs_close();}?>
